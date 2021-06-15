85368ffdbce82b3dcbbef134ec65a7aeec231bd8
alexlignitz <alexlignitz@gmail.com>
2021-06-15 19:06:19 +0200

    Connect basic views to the existing templates

--- CharityApp/settings.py
+++ CharityApp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'charity_app.apps.CharityAppConfig',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'CharityApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [os.path.join(BASE_DIR,'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- charity_app/views.py
+++ charity_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LadingPageView(View):
+    def get(self, request):
+        return render(request, '__base__.html')
+
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, 'form.html')
+
+
