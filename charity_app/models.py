681f63fde70342aef6e7cc121f1c3ee372677b57
alexlignitz <alexlignitz@gmail.com>
2021-06-21 21:12:43 +0200

    Add models and migrations

--- charity_app/models.py
+++ charity_app/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=28)
+
+
+class Institution(models.Model):
+    FOUNDATION = 1
+    NGO = 2
+    LOCAL = 3
+    TYPE_CHOICES = [
+        (FOUNDATION, 'Fundacja'),
+        (NGO, 'Organizacja pozarządowa'),
+        (LOCAL, 'Zbiórka lokalna'),
+    ]
+
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPE_CHOICES)
+    categories = models.ManyToManyField(Category)
+
+class Donation(models.Model):
+    quantity = models.SmallIntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=128)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
