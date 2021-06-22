ff10143cf87f6e362bd263c99165ef7bc92800a6
alexlignitz <alexlignitz@gmail.com>
2021-06-22 18:44:19 +0200

    Add admin page

--- CharityApp/urls.py
+++ CharityApp/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from charity_app.views import LadingPageView, AddDonationView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('accounts/', include('accounts.urls')),
     path('', LadingPageView.as_view(), name="landing"),
     path('add_donation', AddDonationView.as_view(), name="add_donation"),
