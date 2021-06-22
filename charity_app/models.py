abd43b05831841a48ff0115ffc3300a397e4be1d
alexlignitz <alexlignitz@gmail.com>
2021-06-22 20:42:06 +0200

    Add charity list to index.html

--- charity_app/models.py
+++ charity_app/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=28)
 
+    def __str__(self):
+        return self.name
+
 
 class Institution(models.Model):
     FOUNDATION = 1
@@ -20,6 +23,9 @@ class Institution(models.Model):
     description = models.TextField()
     type = models.IntegerField(choices=TYPE_CHOICES)
     categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
 
 
 class Donation(models.Model):
--- charity_app/views.py
+++ charity_app/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.views import View
 
+from charity_app.models import Institution, Category
+
 
 class LadingPageView(View):
     def get(self, request):
-        return render(request, '__base__.html')
+        foundations = Institution.objects.filter(type=1)
+        ngos = Institution.objects.filter(type=2)
+        local_charities = Institution.objects.filter(type=3)
+        categories = Category.objects.all()
+        return render(request, '__base__.html',
+                      {'foundations': foundations, 'ngos': ngos, 'local_charities': local_charities,
+                       'categories': categories})
 
 
 class AddDonationView(View):
     def get(self, request):
         return render(request, 'form.html')
-
-
