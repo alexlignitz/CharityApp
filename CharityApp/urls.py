059d51e9bacb4d5e74c9dc0e93919a8d28d13319
alexlignitz <alexlignitz@gmail.com>
2021-06-29 15:11:40 +0200

    Add donation deletion and status change

--- CharityApp/urls.py
+++ CharityApp/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from charity_app.views import LadingPageView, AddDonationView, DonationDetailsView
+from charity_app.views import LadingPageView, AddDonationView, DonationDetailsView, DeleteDonationView, \
+    DonationTakenView, DonationNotTakenView
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
@@ -9,4 +10,7 @@ urlpatterns = [
     path('', LadingPageView.as_view(), name="landing"),
     path('add_donation/', AddDonationView.as_view(), name="add_donation"),
     path('donation_details/<int:id>/', DonationDetailsView.as_view(), name="donation_details"),
+    path('donation_delete/<int:id>/', DeleteDonationView.as_view(), name="donation_delete"),
+    path('donation_taken/<int:id>/', DonationTakenView.as_view(), name="donation_taken"),
+    path('donation_not_taken/<int:id>/', DonationNotTakenView.as_view(), name="donation_not_taken"),
 ]
--- charity_app/views.py
+++ charity_app/views.py
@@ -53,5 +53,44 @@ class DonationDetailsView(View):
         donation = Donation.objects.get(id=id)
         return render(request, 'donation_details.html', {'donation': donation})
 
+
+class DeleteDonationView(View):
+    def get(self, request, id):
+        return render(request, "change_confirmation.html", {
+            'msg': 'Czy na pewno chcesz zrezygnować z darowizny? Termin odbioru zostanie wtedy usunięty z naszego kalendarza.'})
+
     def post(self, request, id):
-        pass
+        user = request.user
+        if request.POST.get('answer') == 'Tak':
+            donation = Donation.objects.get(id=id)
+            donation.delete()
+            return redirect('my_account', id=user.id)
+        return redirect('donation_details', id=id)
+
+
+class DonationTakenView(View):
+    def get(self, request, id):
+        return render(request, "change_confirmation.html", {
+            'msg': 'Czy darowizna została odebrana? '})
+
+    def post(self, request, id):
+        if request.POST.get('answer') == 'Tak':
+            donation = Donation.objects.get(id=id)
+            donation.is_taken = True
+            donation.save()
+            return redirect('donation_details', id=id)
+        return redirect('donation_details', id=id)
+
+
+class DonationNotTakenView(View):
+    def get(self, request, id):
+        return render(request, "change_confirmation.html", {
+            'msg': 'Czy chcesz zmienić status darowizny na Nieodebrana? Nasza obsługa klienta skontaktuje się wtedy z Tobą w celu ustalenia nowego terminu odbioru'})
+
+    def post(self, request, id):
+        if request.POST.get('answer') == 'Tak':
+            donation = Donation.objects.get(id=id)
+            donation.is_taken = False
+            donation.save()
+            return redirect('donation_details', id=id)
+        return redirect('donation_details', id=id)
